711615e7a70c1f5182ba586b20dc5c1a8980f594
Chris Kuehl <chris@techxonline.net>
2012-02-23 20:03:45 +0000

    Fixing an issue with stale connections in the MySQL pool. #REDEPLOY

--- src/stocksim/grails-app/conf/DataSource.groovy
+++ src/stocksim/grails-app/conf/DataSource.groovy
@@ -2,6 +2,21 @@ dataSource {
     pooled = true
     driverClassName = "com.mysql.jdbc.Driver"
     dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+    
+    // resolve an issue with stale connections and Grails due to automatic
+    // connection pooling done by Grails (enabled above)
+    // http://stackoverflow.com/questions/2740987/mysql-connection-timeout-issue-grails-application-on-tomcat-using-hibernate-an
+    
+    // run the evictor every 30 minutes and evict any connections older than 30 minutes.
+    minEvictableIdleTimeMillis = 1800000
+    timeBetweenEvictionRunsMillis = 1800000
+    numTestsPerEvictionRun = 3
+    
+    // test the connection while its idle, before borrow and return it
+    testOnBorrow = true
+    testWhileIdle = true
+    testOnReturn = true
+    validationQuery = "SELECT 1"
 }
 
 dataSource_temp { // used for ephemeral storage
