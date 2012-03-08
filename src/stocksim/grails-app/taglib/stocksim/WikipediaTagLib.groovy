67d97f4f7a17221291ab728a49a7015e6cb24608
Chris Kuehl <chris@techxonline.net>
2012-03-08 04:38:06 -0500

    Now caching the formatted Wikipedia snippets to speed up page loading.

--- src/stocksim/grails-app/taglib/stocksim/WikipediaTagLib.groovy
+++ src/stocksim/grails-app/taglib/stocksim/WikipediaTagLib.groovy
@@ -11,26 +11,30 @@ class WikipediaTagLib {
         
         if (source == null) { // it wasn't in the cache
             source = wikipediaService.getArticleSource(title)
-            cacheService.storeInCache("wikipedia", title, source)
+            
+            if (source != null) {
+                def content = wikipediaService.getSummaryFromHTML(wikipediaService.cleanupHTML(source)).replaceAll("<(.|\n)*?>", "")
+
+                if (attrs.maxChars == null || content.length() <= attrs.maxChars.toInteger()) {
+                    source = content
+                } else {
+                    def trimmedContent = content.substring(0, attrs.maxChars.toInteger())
+                    trimmedContent = trimmedContent.substring(0, trimmedContent.lastIndexOf(" "))
+
+                    if (trimmedContent.endsWith(",") || trimmedContent.endsWith(".")) {
+                        trimmedContent = trimmedContent.substring(0, trimmedContent.length() - 1)
+                    }
+
+                    trimmedContent += "..."
+                    source = trimmedContent
+                }
+
+                cacheService.storeInCache("wikipedia", title, source)
+            }
         }
         
         if (source != null) {
-            def content = wikipediaService.getSummaryFromHTML(wikipediaService.cleanupHTML(source)).replaceAll("<(.|\n)*?>", "")
-            
-            if (attrs.maxChars == null || content.length() <= attrs.maxChars.toInteger()) {
-                out << content
-            } else {
-                def trimmedContent = content.substring(0, attrs.maxChars.toInteger())
-                trimmedContent = trimmedContent.substring(0, trimmedContent.lastIndexOf(" "))
-                
-                if (trimmedContent.endsWith(",") || trimmedContent.endsWith(".")) {
-                    trimmedContent = trimmedContent.substring(0, trimmedContent.length() - 1)
-                }
-                
-                trimmedContent += "..."
-                out << trimmedContent
-            }
-            
+            out << source
             out << " <a target=\"_blank\" href=\"${wikipediaService.getURL(title)}\">(read more)</a>"
             out << body()
         }
