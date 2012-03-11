72391ab4ecfeec9d569bfda9e11efca4cd137a0f
Chris Kuehl <chris@techxonline.net>
2012-03-11 07:43:36 -0400

    Adding back some URL mappings.

--- src/stocksim/grails-app/conf/UrlMappings.groovy
+++ src/stocksim/grails-app/conf/UrlMappings.groovy
@@ -1,21 +1,33 @@
 class UrlMappings {
-
-	static mappings = {
-                "/search" (controller: "search", action: "browse")
-                "/search/json" (controller: "search", action: "json")
-                
-		"/"(view:"/index")
+    static mappings = {
+        // home page
+        "/" (view:"/index")
         
-                name stock: "/stock/$ticker" {
-                    view = "/stock"
-                }
-                
-                name faq: "/faq" {
-                    view = "/faq"
-                }
-                
-                // errors
-		"500"(view:'/error')
-		"404"(view:'/error')
-	}
+        // search
+        "/search" (controller: "search", action: "browse")
+        "/search/json" (controller: "search", action: "json")
+        
+        // stock page
+        name stock: "/stock/$ticker" {
+            view = "/stock"
+        }
+        
+        // content pages
+        name faq: "/faq" {
+            view = "/faq"
+        }
+        
+        // admin pages
+        "/admin/cache/$action" (controller: "adminCache")
+        
+        // dev pages
+        "/dev/googleNews/$action" (controller: "googleNews")
+        "/dev/sandbox/$action" (controller: "sandbox")
+        "/dev/searchableStock/$action/$id?" (controller: "searchableStock")
+        "/dev/wikipedia/$action" (controller: "wikipedia")
+
+        // errors
+        "500"(view:'/error')
+        "404"(view:'/error')
+    }
 }
