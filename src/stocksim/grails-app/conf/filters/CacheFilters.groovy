10aba53a0419d3f18feacb969ef9932fbe9ceae9
Chris Kuehl <chris@techxonline.net>
2012-04-24 18:24:53 +0000

    Sending an "expires" header when requesting pages to prevent odd results.

---
+++ src/stocksim/grails-app/conf/filters/CacheFilters.groovy
@@ -0,0 +1,17 @@
+package filters
+
+class CacheFilters {
+    def filters = {
+        noCachingFilter(uri: "/**") {
+            before = {
+                // some browsers are eager to cache pages (especially on
+                // the iPad and some other mobile devices we've tested)
+                // so we do this to specifically tell them not to in order to
+                // prevent weird results (e.g. signing up and still appearing
+                // to not be logged in)
+                response.setHeader('Expires', '-1')
+            }
+        }
+    }
+}
+
