a22dda943164f2930d1b1850f7196723193a9890
Chris Kuehl <chris@techxonline.net>
2012-04-25 17:25:04 -0400

    Temporarily re-enable admin on prod.

--- src/stocksim/grails-app/conf/filters/AdminFilters.groovy
+++ src/stocksim/grails-app/conf/filters/AdminFilters.groovy
@@ -5,8 +5,8 @@ class AdminFilters {
         noCachingFilter(uri: "/admin/**") {
             before = {
                 if (grails.util.GrailsUtil.getEnvironment().equals(org.codehaus.groovy.grails.commons.GrailsApplication.ENV_PRODUCTION)) {
-                    render(view: "/denied")    
-                    return false
+//                    render(view: "/denied")    
+//                    return false
                 }
             }
         }
