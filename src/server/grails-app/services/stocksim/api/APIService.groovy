84a6c548f7be6c6911bcbcb931e4816f90c6ed18
Chris Kuehl <chris@techxonline.net>
2012-08-11 01:31:21 -0400

    Using alternative method of loading file so it works in production (TODO: fix searchService to do this).

--- src/server/grails-app/services/stocksim/api/APIService.groovy
+++ src/server/grails-app/services/stocksim/api/APIService.groovy
@@ -5,11 +5,13 @@ class APIService {
     
     def generateResponse(def category, def item, def action, def params) {
         def response = [:]
+        def startTime = (new Date()).getTime()
         
         // process for a response
         
         // add standard stuff on all requests
-        response.apiTime = (new Date()).toString()
+        response.apiDate = (new Date()).toString()
+        response.apiTime = (new Date()).getTime() - startTime
         response.apiLyric = lyricService.getLine()
         
         // feed back the response so it can be processed and output by some controller
--- src/server/grails-app/services/stocksim/api/LyricService.groovy
+++ src/server/grails-app/services/stocksim/api/LyricService.groovy
@@ -1,9 +1,8 @@
 package stocksim.api
 
-import grails.util.BuildSettingsHolder as BSH
-
 class LyricService {
     def cacheService
+    def grailsApplication
     def currentSong = "cat-in-the-hat"
     
     def getLine() {
@@ -18,8 +17,7 @@ class LyricService {
     }
     
     def refreshCache(def song) {
-        def baseDir = BSH.getSettings().baseDir
-        def lyricFile = new File("${baseDir}/grails-app/conf/lyrics/${song}.txt")
+        def lyricFile = grailsApplication.parentContext.getResource("lyrics/${song}.txt").file
         def lines = lyricFile.readLines()
         
         cacheService.storeInCache("lyrics", song, lines)
