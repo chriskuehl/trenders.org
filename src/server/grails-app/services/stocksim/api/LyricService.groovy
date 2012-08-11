27c99a5de70b63b48ef3623b9714b70eafecb0b4
Chris Kuehl <chris@techxonline.net>
2012-08-11 01:13:01 -0400

    Now providing a lyrical line for each API request, free of charge, regardless of whether or not the requester wants it :-).

--- src/server/grails-app/services/stocksim/api/LyricService.groovy
+++ src/server/grails-app/services/stocksim/api/LyricService.groovy
@@ -1,9 +1,27 @@
 package stocksim.api
 
+import grails.util.BuildSettingsHolder as BSH
+
 class LyricService {
     def cacheService
+    def currentSong = "cat-in-the-hat"
     
     def getLine() {
-        "Testing" //
+        def lines = cacheService.fetchFromCache("lyrics", currentSong, 0)
+        
+        if (lines == null) {
+            refreshCache(currentSong)
+            return getLine()
+        }
+        
+        lines[(int) Math.floor(Math.random() * lines.size())]
+    }
+    
+    def refreshCache(def song) {
+        def baseDir = BSH.getSettings().baseDir
+        def lyricFile = new File("${baseDir}/grails-app/conf/lyrics/${song}.txt")
+        def lines = lyricFile.readLines()
+        
+        cacheService.storeInCache("lyrics", song, lines)
     }
 }
