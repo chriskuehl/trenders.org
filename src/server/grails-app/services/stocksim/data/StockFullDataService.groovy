5ce1f087bad67f2109922b58e1e1f0d826513e1f
Chris Kuehl <chris@techxonline.net>
2012-09-02 19:46:59 -0400

    Getting ticker list in stockFullDataService.

--- src/server/grails-app/services/stocksim/data/StockFullDataService.groovy
+++ src/server/grails-app/services/stocksim/data/StockFullDataService.groovy
@@ -1,7 +1,10 @@
 package stocksim.data
 
 class StockFullDataService {
+    def stockDataService
+    
+    // for each existing ticker, we need to get the info
     def update() {
-        
+        def tickerList = stockDataService.getTickerList()
     }
 }
