7ad91abc7d31e778576ec0bad8a331de52d9fc9b
Chris Kuehl <chris@techxonline.net>
2012-09-02 20:22:08 -0400

    Creating batching pattern for handling stock ticker list.

--- src/server/grails-app/services/stocksim/data/StockFullDataService.groovy
+++ src/server/grails-app/services/stocksim/data/StockFullDataService.groovy
@@ -1,10 +1,45 @@
 package stocksim.data
 
 class StockFullDataService {
-    def stockDataService
+    final def batchSize = 20
     
     // for each existing ticker, we need to get the info
     def update() {
+        def stockDataService
         def tickerList = stockDataService.getTickerList()
+        
+        // split into batches of 20
+        def batches = splitListIntoSubListsOfSize(tickerList, batchSize)
+        
+        // iterate through each batch and fetch the new data
+        batches.each { batch ->
+            // example URL: http://download.finance.yahoo.com/d/quotes.csv?s=RHT,MSFT,NOV&f=sl1d1t1c1ohgv&e=.csv
+            println batch
+        }
+    }
+    
+    def splitListIntoSubListsOfSize(def list, def listSize) {
+        def lists = [[]]
+        
+        // now add each item to a sublist
+        list.each { item ->
+            // add to the last sublist
+            lists[lists.size() - 1].add(list[0])
+            
+            // is the last sublist now full?
+            if (lists[lists.size() - 1].size() >= listSize) {
+                // it is, so add a new sublist
+                lists.add([])
+            }
+        }
+        
+        // is the last sublist empty? then remove it
+        def lastListIndex = lists.size() - 1
+        
+        if (lists[lastListIndex].size() <= 0) {
+            lists.remove(lastListIndex)
+        }
+        
+        lists
     }
 }
