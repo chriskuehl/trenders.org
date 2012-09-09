e890e59936c64a8551ef6be4d18a3fcebb9d25a5
Chris Kuehl <chris@techxonline.net>
2012-09-09 12:13:01 -0400

    Not bothering to update market data when the markets are definitely closed.

--- src/server/grails-app/services/stocksim/data/StockDataService.groovy
+++ src/server/grails-app/services/stocksim/data/StockDataService.groovy
@@ -1,6 +1,9 @@
 package stocksim.data
 
 import stocksim.*
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import org.joda.time.DateTimeConstants
 
 class StockDataService {
     def stockDataListService
@@ -9,11 +12,24 @@ class StockDataService {
     def listReady = false
     def dataReady = false
     
+    def lastCheckMarketsClosed = false
+    
     def isReady() {
         listReady && dataReady
     }
     
     def updateStockData() {
+        def marketOpen = marketIsOpen()
+        
+        if (! marketOpen && lastCheckMarketsClosed) {
+            println "Market still closed, not updating."
+            return
+        } else if (! marketOpen) {
+            println "Market closed, but was open last check, going ahead with update..."
+        }
+        
+        lastCheckMarketsClosed = ! marketOpen
+        
         println "Updating stock data..."
         
         // update all the data
@@ -32,6 +48,25 @@ class StockDataService {
             println "Unable to fetch full stock data, failed to load stock list; SITE IS DOWN!"
             // TODO: speed up retrieval next time since site is down!
         }
+    }
+    
+    def marketIsOpen() {
+        def currentTime = new DateTime(DateTimeZone.forID("America/New_York"))
+        def currentHour = currentTime.getHourOfDay()
+        def currentDayOfWeek = currentTime.getDayOfWeek()
+        
+        if (! (DateTimeConstants.MONDAY..DateTimeConstants.FRIDAY).contains(currentDayOfWeek)) {
+            println "Markets closed, bad day of the week"
+            return false
+        }
+        
+        if (! (9..16).contains(currentHour)) {
+            println "Markets closed, bad hour"
+            return false
+        }
+        
+        println "Markets probably open"
+        return true
     }
     
     def updateStockList() {
