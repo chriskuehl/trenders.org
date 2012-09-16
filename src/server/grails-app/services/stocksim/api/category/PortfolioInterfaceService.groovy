b4c804c8c1bf85e0e5d3075e5e2e2d31c15222eb
Chris Kuehl <chris@techxonline.net>
2012-09-15 20:54:29 -0400

    Adding portfolio/_current API call.

--- src/server/grails-app/services/stocksim/api/category/PortfolioInterfaceService.groovy
+++ src/server/grails-app/services/stocksim/api/category/PortfolioInterfaceService.groovy
@@ -5,6 +5,37 @@ import stocksim.*
 class PortfolioInterfaceService {
     def financeService
     
+    public def _current = { response, action, params, user ->
+        def assets = []
+        
+        user.getOwnedStocks().each { ownedStock ->
+            def stock = financeService.getStock(ownedStock.getTicker())
+            
+            assets.add([
+                company: [
+                    ticker: stock.ticker,
+                    name: stock.name,
+                    lastSale: stock.lastSale,
+                    marketCap: stock.marketCap,
+                    ipoYear: stock.ipoYear,
+                    sector: stock.sector,
+                    industry: stock.industry,
+                    exchange: stock.exchange
+                ],
+                
+                numShares: ownedStock.quantity,
+                totalSpent: ownedStock.totalSpent.toDouble().trunc(2),
+                currentValue: stock.lastSale * ownedStock.quantity,
+                gain: ((stock.lastSale * ownedStock.quantity) - ownedStock.totalSpent.toDouble()).round(2)
+            ])
+        }
+        
+        response.totalSpent = user.getPrettyMoneySpentOnPortfolio()
+        response.totalWorth = user.getPrettyPortfolioValue()
+        
+        response.assets = assets
+    }
+    
     public def _history = { response, action, params, user ->
         def events = []
         
