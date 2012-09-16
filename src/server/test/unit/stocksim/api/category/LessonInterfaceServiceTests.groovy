79c44557ab9d3afdee530728b1dac1c171cde4c9
Chris Kuehl <chris@techxonline.net>
2012-09-16 13:45:09 -0400

    Adding lesson/list and lesson/read API calls for getting information about lessons.

---
+++ src/server/test/unit/stocksim/api/category/LessonInterfaceServiceTests.groovy
@@ -0,0 +1,17 @@
+package stocksim.api.category
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(LessonInterfaceService)
+class LessonInterfaceServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
