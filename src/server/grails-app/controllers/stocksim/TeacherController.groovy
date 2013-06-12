e1d30ad6fd295c7a21b4b5f94454dd74495d2a2d
Chris Kuehl <chris@techxonline.net>
2013-06-11 23:38:54 -0300

    Add interceptor to teacher section to prevent students from accessing the teacher portal.

--- src/server/grails-app/controllers/stocksim/TeacherController.groovy
+++ src/server/grails-app/controllers/stocksim/TeacherController.groovy
@@ -1,6 +1,13 @@
 package stocksim
 
 class TeacherController {
+    def beforeInterceptor = {
+        if (! request.user || ! (request.user.isTeacher() || request.user.isAdmin()) {
+            render "You are not authorized to view this page."
+            return false
+        }
+    }
+
     def updateInformation() {
         if (params.dataType == "updateEmail") {
             def user = User.findByEmail(params.email)
