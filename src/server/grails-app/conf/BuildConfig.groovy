2345cee1a0b421b3beae6d38f3bc4d468519667d
Chris Kuehl <chris@techxonline.net>
2013-09-21 09:35:37 -0700

    Initial changes to start the upgrade process to 2.3.0.

--- src/server/grails-app/conf/BuildConfig.groovy
+++ src/server/grails-app/conf/BuildConfig.groovy
@@ -6,6 +6,7 @@ grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -14,16 +15,18 @@ grails.project.dependency.resolution = {
     }
     log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
+	legacyResolve false
 
     repositories {
         inherits true // Whether to inherit repository definitions from plugins
+
         grailsPlugins()
         grailsHome()
         grailsCentral()
         mavenCentral()
 
         // uncomment these to enable remote dependency resolution from public Maven repositories
-        mavenCentral()
+        //mavenCentral()
         //mavenLocal()
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
@@ -33,7 +36,7 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        runtime "mysql:mysql-connector-java:5.1.18"
+        runtime "mysql:mysql-connector-java:5.1.26"
         runtime "net.sf.opencsv:opencsv:2.0"
         runtime "joda-time:joda-time:2.1"
         runtime "com.ocpsoft:ocpsoft-pretty-time:1.0.7"
@@ -41,9 +44,9 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
+        runtime ":hibernate:3.6.10.1"
         runtime ":resources:1.1.5"
 
-        build ":tomcat:$grailsVersion"
+        build ":tomcat:7.0.42"
     }
 }
